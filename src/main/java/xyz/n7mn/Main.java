fc4d6ea79aa8dc622bacd25dc1b9802b3d201d45
ちのなみ <tino_nanami@7mi.site>
2023-05-19 03:02:30 +0900

    公式動画のhlsオンリーに一応対応させておく

--- src/main/java/xyz/n7mn/Main.java
+++ src/main/java/xyz/n7mn/Main.java
@@ -302,9 +302,33 @@ public class Main {
 
         Matcher matcher4 = Pattern.compile("archive_h264_600kbps_360p").matcher(HtmlText);
         Matcher matcher5 = Pattern.compile("archive_h264_300kbps_360p").matcher(HtmlText);
+        Matcher matcher_http = Pattern.compile("&quot;http&quot;").matcher(HtmlText);
 
         String json = "{\"session\":{\"recipe_id\":\"nicovideo-"+id+"\",\"content_id\":\"out1\",\"content_type\":\"movie\",\"content_src_id_sets\":[{\"content_src_ids\":[{\"src_id_to_mux\":{\"video_src_ids\":["+(matcher5.find() ? "\"archive_h264_300kbps_360p\"" : (matcher4.find() ? "\"archive_h264_600kbps_360p\",\"archive_h264_300kbps_360p\"" : "\"archive_h264_360p\",\"archive_h264_360p_low\""))+"],\"audio_src_ids\":[\"archive_aac_64kbps\"]}}]}],\"timing_constraint\":\"unlimited\",\"keep_method\":{\"heartbeat\":{\"lifetime\":120000}},\"protocol\":{\"name\":\"http\",\"parameters\":{\"http_parameters\":{\"parameters\":{\"http_output_download_parameters\":{\"use_well_known_port\":\"yes\",\"use_ssl\":\"yes\",\"transfer_preset\":\"\"}}}}},\"content_uri\":\"\",\"session_operation_auth\":{\"session_operation_auth_by_signature\":{\"token\":\""+Token+"\",\"signature\":\""+Signature+"\"}},\"content_auth\":{\"auth_type\":\"ht2\",\"content_key_timeout\":600000,\"service_id\":\"nicovideo\",\"service_user_id\":\""+SessionId+"\"},\"client_info\":{\"player_id\":\"nicovideo-"+SessionId+"\"},\"priority\":0"+(id.startsWith("so") ? ".2" : "")+"}}";
+        if (!matcher_http.find()){
+            Matcher matcher_hls1 = Pattern.compile("hls_encrypted_key\\\\&quot;:\\\\&quot;(.*)\\\\&quot;}&quot;,&quot;signature").matcher(HtmlText);
+            Matcher matcher_hls2 = Pattern.compile("&quot;keyUri&quot;:&quot;(.*)&quot;},&quot;movie").matcher(HtmlText);
+            Matcher matcher_hls3 = Pattern.compile(",&quot;token&quot;:&quot;(.*)&quot;,&quot;signature&quot;:&quot;").matcher(HtmlText);
 
+            String hls_encrypted_key = "";
+            if (matcher_hls1.find()){
+                hls_encrypted_key = matcher_hls1.group(1).replaceAll("\\\\","");
+            }
+            String keyUri = "";
+            if (matcher_hls2.find()){
+                keyUri = matcher_hls2.group(1).replaceAll("\\\\","").replaceAll("&amp;","&");
+            }
+
+            if (matcher_hls3.find()){
+                Token = matcher_hls3.group(1).replaceAll("&quot;","\"");
+            }
+
+            //System.out.println(hls_encrypted_key);
+            //System.out.println(keyUri);
+            //System.out.println(SessionId);
+
+            json = "{\"session\":{\"recipe_id\":\"nicovideo-"+id+"\",\"content_id\":\"out1\",\"content_type\":\"movie\",\"content_src_id_sets\":[{\"content_src_ids\":[{\"src_id_to_mux\":{\"video_src_ids\":[\"archive_h264_360p\",\"archive_h264_360p_low\"],\"audio_src_ids\":[\"archive_aac_64kbps\"]}},{\"src_id_to_mux\":{\"video_src_ids\":[\"archive_h264_360p_low\"],\"audio_src_ids\":[\"archive_aac_64kbps\"]}}]}],\"timing_constraint\":\"unlimited\",\"keep_method\":{\"heartbeat\":{\"lifetime\":120000}},\"protocol\":{\"name\":\"http\",\"parameters\":{\"http_parameters\":{\"parameters\":{\"hls_parameters\":{\"use_well_known_port\":\"yes\",\"use_ssl\":\"yes\",\"transfer_preset\":\"\",\"segment_duration\":6000,\"encryption\":{\"hls_encryption_v1\":{\"encrypted_key\":\""+hls_encrypted_key+"\",\"key_uri\":\""+keyUri+"\"}}}}}}},\"content_uri\":\"\",\"session_operation_auth\":{\"session_operation_auth_by_signature\":{\"token\":\""+Token+"\",\"signature\":\""+Signature+"\"}},\"content_auth\":{\"auth_type\":\"ht2\",\"content_key_timeout\":600000,\"service_id\":\"nicovideo\",\"service_user_id\":\""+SessionId+"\"},\"client_info\":{\"player_id\":\"nicovideo-"+SessionId+"\"},\"priority\":0.2}}";
+        }
 
 
         //System.out.println(json);
@@ -365,6 +389,7 @@ public class Main {
         //System.out.println("[Debug] 動画情報 取得成功\n動画URL : "+VideoURL+" \n"+ sdf.format(new Date()));
         LogRedisWrite(AccessCode, "getURL:success",VideoURL);
         System.gc();
+        //System.out.println(VideoURL);
 
         // 最低限動画の長さ分だけハートビート信号投げつける (40秒起き)
         long finalTime = time;
@@ -552,9 +577,9 @@ public class Main {
 
             @Override
             public void onMessage(@NotNull WebSocket webSocket, @NotNull String text) {
-                System.out.println("---- result text ----");
-                System.out.println(text);
-                System.out.println("---- result text ----");
+                //System.out.println("---- result text ----");
+                //System.out.println(text);
+                //System.out.println("---- result text ----");
 
                 if (text.startsWith("{\"type\":\"serverTime\",\"data\":{")){
                     webSocket.send("{\"type\":\"getEventState\",\"data\":{}}");
