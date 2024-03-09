ced77467e2deffc9f0114a38b5680b59665e9489
ちのなみ <tino_nanami@7mi.site>
2024-03-09 11:46:27 +0900

    処理鯖から応答なかった場合別の処理鯖に見に行かないのを修正

--- src/main/java/net/nicovrc/dev/api/ConversionAPI.java
+++ src/main/java/net/nicovrc/dev/api/ConversionAPI.java
@@ -27,7 +27,7 @@ import java.util.regex.Pattern;
 
 public class ConversionAPI {
 
-    private static final String ver = "2.0-20240308";
+    private static final String ver = "2.0-20240309";
 
     private final ProxyAPI proxyAPI;
     private final String SocketIP;
