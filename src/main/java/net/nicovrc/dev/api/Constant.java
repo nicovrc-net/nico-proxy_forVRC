a45069b9ebeeff4dd42c724858f0e29d3fba1f45
ちのなみ <tino_nanami@7mi.site>
2024-05-23 23:18:56 +0900

    SoundCloudで再生できない音楽があるのを修正

--- src/main/java/net/nicovrc/dev/api/Constant.java
+++ src/main/java/net/nicovrc/dev/api/Constant.java
@@ -2,7 +2,7 @@ package net.nicovrc.dev.api;
 
 public class Constant {
 
-    private static final String Version = "2.5.4-beta";
+    private static final String Version = "2.5.4";
     private static final String UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0 nicovrc/"+Version;
 
     public static String getVersion(){
