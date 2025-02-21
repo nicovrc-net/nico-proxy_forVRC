d324df72675a9b0760baa58bd84866ca58685536
Chino Nanami <tino_nanami@7mi.site>
2025-02-21 10:58:43 +0900

    [ci skip] update

--- src/main/java/net/nicovrc/dev/TCPServer.java
+++ src/main/java/net/nicovrc/dev/TCPServer.java
@@ -1,9 +1,68 @@
 package net.nicovrc.dev;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class TCPServer extends Thread {
+
+    private final int HTTPPort;
+    private final boolean[] temp = {true};
+
+    public TCPServer(){
+        this.HTTPPort = 25252;
+    }
 
     @Override
     public void run() {
+        ServerSocket svSock = null;
+        try {
+            svSock = new ServerSocket(HTTPPort);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
+        System.out.println("[Info] TCP Port " + HTTPPort + "で 処理受付用HTTPサーバー待機開始");
+        while (temp[0]) {
+            try {
+                //System.gc();
+                //System.out.println("[Debug] HTTPRequest待機");
+                final Socket sock = svSock.accept();
+                Thread.ofVirtual().start(() -> {
+                    try {
+                        final InputStream in = sock.getInputStream();
+                        final OutputStream out = sock.getOutputStream();
+
+                        StringBuffer sb = new StringBuffer();
+                        byte[] data = new byte[1024];
+                        int readSize = in.read(data);
+
+                        if (readSize <= 0) {
+                            in.close();
+                            out.close();
+                            sock.close();
+                            return;
+                        }
+                        data = Arrays.copyOf(data, readSize);
+                        sb.append(new String(data, StandardCharsets.UTF_8));
+                    } catch (Exception e) {
+
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+                temp[0] = false;
+            }
+        }
+        try {
+            svSock.close();
+            svSock = null;
+        } catch (Exception e){
+            // e.printStackTrace();
+        }
     }
 }
