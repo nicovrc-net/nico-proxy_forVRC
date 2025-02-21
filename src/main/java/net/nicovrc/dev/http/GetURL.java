03af61098e81a7100ddf6937040351ca352ddd0b
Chino Nanami <tino_nanami@7mi.site>
2025-02-21 17:34:43 +0900

    仮置き

--- src/main/java/net/nicovrc/dev/http/GetURL.java
+++ src/main/java/net/nicovrc/dev/http/GetURL.java
@@ -1,11 +1,28 @@
 package net.nicovrc.dev.http;
 
+import net.nicovrc.dev.Function;
+
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class GetURL implements Runnable, NicoVRCHTTP {
 
+    private Socket sock = null;
+    private String URL = null;
+    private String httpRequest = null;
+
     @Override
     public void run() {
+
+        StringBuilder sb = new StringBuilder("--- dummy ---\n");
+        sb.append(httpRequest);
+
+        String method = Function.getMethod(httpRequest);
+        try {
+            Function.sendHTTPRequest(sock, Function.getHTTPVersion(httpRequest), 200, "text/plain; charset=utf-8", sb.toString().getBytes(StandardCharsets.UTF_8), method != null && method.equals("HEAD"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -16,16 +33,16 @@ public class GetURL implements Runnable, NicoVRCHTTP {
 
     @Override
     public void setHTTPRequest(String httpRequest) {
-
+        this.httpRequest = httpRequest;
     }
 
     @Override
     public void setURL(String URL) {
-
+        this.URL = URL;
     }
 
     @Override
     public void setHTTPSocket(Socket sock) {
-
+        this.sock = sock;
     }
 }
