63324e8f1e390509d2b900faf46bc1f3c5fc9d74
Chino Nanami <tino_nanami@7mi.site>
2025-02-24 14:59:04 +0900

    不要なの削除

--- src/main/java/net/nicovrc/dev/api/GetVersion.java
+++ src/main/java/net/nicovrc/dev/api/GetVersion.java
@@ -1,19 +1,8 @@
 package net.nicovrc.dev.api;
 
 import net.nicovrc.dev.Function;
-import net.nicovrc.dev.Service.NicoVideo;
-import net.nicovrc.dev.Service.ServiceAPI;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class GetVersion implements NicoVRCAPI {
-
-    private final List<ServiceAPI> siteList = new ArrayList<>();
-
-    public GetVersion(){
-        siteList.add(new NicoVideo());
-    }
 
     @Override
     public String getURI() {
