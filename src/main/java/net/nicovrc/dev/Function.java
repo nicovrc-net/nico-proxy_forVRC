f2ee0d7a5f567c27efbbad9f62a646b5d7b1e1f7
Chino Nanami <tino_nanami@7mi.site>
2025-03-01 01:18:23 +0900

    [ci skip] update

--- src/main/java/net/nicovrc/dev/Function.java
+++ src/main/java/net/nicovrc/dev/Function.java
@@ -14,7 +14,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Function {
-    public static final String Version = "3.0.0-alpha.5";
+    public static final String Version = "3.0.0-alpha.6";
     public static final Gson gson = new Gson();
     public static final String UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0 nicovrc-net/" + Version;
 
