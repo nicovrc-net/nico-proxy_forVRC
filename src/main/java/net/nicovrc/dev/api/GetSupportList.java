c2f6ae39902665fb92418e72cfdb92fc700846fa
Chino Nanami <tino_nanami@7mi.site>
2025-03-02 14:24:22 +0900

    bugfix

--- src/main/java/net/nicovrc/dev/api/GetSupportList.java
+++ src/main/java/net/nicovrc/dev/api/GetSupportList.java
@@ -6,10 +6,13 @@ import net.nicovrc.dev.Service.ServiceList;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class GetSupportList implements NicoVRCAPI {
 
     private final List<ServiceAPI> siteList = ServiceList.getServiceList();
+
+    private final Pattern matcher = Pattern.compile("\\.");
 
     @Override
     public String getURI() {
@@ -24,9 +27,24 @@ public class GetSupportList implements NicoVRCAPI {
             if (!value.getServiceName().equals("ニコニコ")){
                 map.put(value.getServiceName(), value.getCorrespondingURL());
             } else {
-                if (value.getCorrespondingURL().length != 2){
-                    map.put(value.getServiceName(), value.getCorrespondingURL());
+
+                String[] tempList = null;
+                int i = 0;
+                for (String str : value.getCorrespondingURL()){
+                    if (matcher.matcher(str).find()){
+                        i++;
+                    }
                 }
+                tempList = new String[i + 1];
+                i = 0;
+                for (String str : value.getCorrespondingURL()){
+                    if (matcher.matcher(str).find()){
+                        tempList[i] = str;
+                        i++;
+                    }
+                }
+
+                map.put(value.getServiceName(), tempList);
             }
         });
 
