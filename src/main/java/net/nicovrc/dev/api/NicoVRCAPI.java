c2404c0227a2e0fd940f4453bc879ceb27b59e87
Chino Nanami <tino_nanami@7mi.site>
2025-12-03 19:10:50 +0900

    init commit

--- src/main/java/net/nicovrc/dev/api/NicoVRCAPI.java
+++
@@ -1,9 +0,0 @@
-package net.nicovrc.dev.api;
-
-
-public interface NicoVRCAPI {
-
-    String getURI();
-    String Run(String httpRequest) throws Exception;
-
-}
--- src/main/java/net/nicovrc/dev/http/getContent/GetContent.java
+++
@@ -1,9 +0,0 @@
-package net.nicovrc.dev.http.getContent;
-
-import java.net.http.HttpClient;
-
-public interface GetContent {
-
-    ContentObject run(HttpClient client, String httpRequest, String URL, String json) throws Exception;
-
-}
