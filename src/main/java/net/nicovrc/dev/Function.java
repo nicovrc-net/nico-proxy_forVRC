446b1fdf88e1264bae7911f65297cc2293594528
Chino Nanami <tino_nanami@7mi.site>
2026-02-28 15:46:22 +0900

    update

--- src/main/java/net/nicovrc/dev/Function.java
+++ src/main/java/net/nicovrc/dev/Function.java
@@ -25,7 +25,7 @@ import java.util.zip.GZIPOutputStream;
 public class Function {
     public static final String Version = "3.1.0-beta.3";
     public static final Gson gson = new Gson();
-    public static final String UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0 nicovrc-net/" + Version;
+    public static final String UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:148.0) Gecko/20100101 Firefox/148.0 nicovrc-net/" + Version;
     public static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public static final byte[] zeroByte = new byte[0];
