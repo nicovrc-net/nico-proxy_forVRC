555caacb08b2a92b1ecf0b8faf7ec3dc922ae429
Chino Nanami <tino_nanami@7mi.site>
2026-07-16 22:28:28 +0900

    update

--- src/main/java/net/nicovrc/dev/TCPServer.java
+++ src/main/java/net/nicovrc/dev/TCPServer.java
@@ -5,6 +5,7 @@ import net.nicovrc.dev.data.HttpHeader;
 import net.nicovrc.dev.http.GetURL;
 import net.nicovrc.dev.http.GetVideo;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.http.HttpClient;
 import java.nio.ByteBuffer;
@@ -77,151 +78,160 @@ public class TCPServer extends Thread {
         System.out.println("[Info] TCP Port " + Function.config_httpPort + "で 処理受付用HTTPサーバー待機開始");
         System.out.println("[Info] VRC動画プレーヤーからはhttp://(サーバーIP):"+Function.config_httpPort+"/?url=(URL)");
 
-        try (AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open()
-                .bind(new InetSocketAddress(Function.config_httpPort))) {
+        try (AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open()){
+            server.bind(new InetSocketAddress(Function.config_httpPort));
             server.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
-                public void completed(AsynchronousSocketChannel ch, Void att) {
+
+                @Override
+                public void completed(AsynchronousSocketChannel clientChannel, Void attachment) {
                     server.accept(null, this);
 
-                    ByteBuffer buf = ByteBuffer.allocate(4096);
-                    ch.read(buf, buf, new CompletionHandler<>() {
-                        public void completed(Integer n, ByteBuffer b) {
-                            if (n == -1) {
-                                close(ch);
-                                return;
-                            }
-                            b.flip();
-                            //System.out.println(new String(b.array(), StandardCharsets.UTF_8));
-                            final String httpRequest = Function.getHTTPRequest(b);
-
-                            //System.out.println(httpRequest);
-
-                            if (httpRequest.isEmpty()) {
-                                close(ch);
-                                return;
-                            }
-
-                            final String httpVersion = Function.getHTTPVersion(httpRequest);
-                            final String httpMethod = Function.getMethod(httpRequest);
-
-                            final boolean isGET = httpMethod != null && httpMethod.equals("GET");
-                            final boolean isPOST = httpMethod != null && httpMethod.equals("POST");
-                            final boolean isHead = httpMethod != null && httpMethod.equals("HEAD");
-
-                            if (!isGET && !isPOST && !isHead) {
-                                //System.out.println("[Debug] HTTPRequest送信");
-                                Function.sendHttpData(ch, new HttpHeader(httpVersion, 405, Function.contentType_textPlain, null, "*", Function.content_MethodNotAllowed, null));
-                                return;
-                            }
-
-                            final String URI = Function.getURI(httpRequest);
-                            if (URI == null) {
-                                //System.out.println("[Debug] HTTPRequest送信");
-                                Function.sendHttpData(ch, new HttpHeader(httpVersion, 502, Function.contentType_textPlain, null, "*", Function.content_BadGateway, null));
-                                System.out.println(httpRequest);
-                                return;
-                            }
-
-                            final Matcher matcher = matcher_uri.matcher(URI);
-                            final boolean ApiMatchFlag = URI.startsWith("/api/");
-                            final boolean UrlMatchFlag = matcher.find();
-                            final boolean VideoMatchFlag = URI.startsWith("/video");
-
-                            Matcher matcher_range1 = matcher_http_range1.matcher(httpRequest);
-                            Matcher matcher_range2 = matcher_http_range2.matcher(httpRequest);
-                            final boolean RangeVideoFlag = matcher_range1.find();
-                            final boolean RangeVideoFullFlag = matcher_range2.find();
-
-                            if (ApiMatchFlag) {
-                                byte[] httpBody = null;
-                                for (NicoVRCAPI api : Function.APIList) {
-                                    if (URI.startsWith(api.getURI())) {
-                                        try {
-                                            httpBody = api.Run(httpRequest, client).getBytes(StandardCharsets.UTF_8);
-                                        } catch (Exception e) {
-                                            throw new RuntimeException(e);
-                                        }
-                                        Function.sendHttpData(ch, new HttpHeader(httpVersion, 200, Function.contentType_json, null, "*", httpBody, null));
-                                        System.out.println("[API (" + Function.sdf.format(new Date()) + ")] " + URI + " ---> OK");
-                                        Function.APIAccessLog.put(UUID.randomUUID().toString(), httpRequest);
-                                        break;
-                                    }
-                                }
-
-                                if (httpBody == null) {
-                                    Function.sendHttpData(ch, new HttpHeader(httpVersion, 404, Function.contentType_textPlain, null, "*", Function.content_errorAPINotFound, null));
-                                    System.out.println("[API (" + Function.sdf.format(new Date()) + ")] " + URI + " ---> Error");
-                                    Function.APIAccessLog.put(UUID.randomUUID().toString(), httpRequest);
-                                }
-
-                                close(ch);
-                                return;
-                            }
-
-                            if (VideoMatchFlag || (RangeVideoFlag && !matcher_range1.group(1).equals("0"))) {
-                                getVideo.setHTTPRequest(httpRequest);
-                                getVideo.setURL(URI);
-                                getVideo.setHTTPSocket(ch);
-
-                                getVideo.run();
-                                close(ch);
-                                return;
-                            }
-
-                            if (UrlMatchFlag || RangeVideoFlag || RangeVideoFullFlag) {
-                                boolean b1 = matcher_uri_vi.matcher(URI).find();
-                                if (URI.startsWith("/proxy")) {
-                                    getURL.setURL(URI.replaceFirst("/proxy/\\?", ""));
-                                } else if (b1) {
-                                    getURL.setURL(URI.replaceFirst("vi=", "url="));
-                                }
-                                getURL.setHTTPRequest(httpRequest);
-
-                                getURL.setHTTPSocket(ch);
-
-                                getURL.run();
-                                close(ch);
-                                return;
-                            }
-
-                            Function.sendHttpData(ch, new HttpHeader(httpVersion, 404, Function.contentType_textPlain, null, "*", Function.content_NotFound, null));
-                            close(ch);
-                        }
-
-                        public void failed(Throwable e, ByteBuffer b) {
-                            close(ch);
-                        }
-                    });
+                    // このクライアントからのデータ読み込み処理を非同期で開始
+                    http(clientChannel);
                 }
 
-                public void failed(Throwable e, Void att) {
-                    e.printStackTrace();
-                }
-
-                void close(AsynchronousSocketChannel c) {
-                    try {
-                        c.close();
-                    } catch (Exception ignored) {
-                    }
+                @Override
+                public void failed(Throwable exc, Void attachment) {
+                    //System.err.println("Accept failed: " + exc.getMessage());
+                    exc.printStackTrace();
                 }
             });
 
-            while (true) {
-                if (Function.isFoundFile("./stop_lock.txt")){
-                    Function.deleteFile("./stop_lock.txt");
-                    break;
-                }
-                try {
-                    Thread.sleep(100L);
-                } catch (Exception ignored) {
-                    //ignored.printStackTrace();
-                }
-            }
-            //Thread.currentThread().join();
-        } catch (Exception e) {
+        } catch (Exception e){
             e.printStackTrace();
         }
 
+        // 3. メインスレッドが即座に終了するのを防ぐため、待機させる
+        try {
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
         Function.checkTimer.cancel();
+    }
+
+    private void http(AsynchronousSocketChannel ch){
+        ByteBuffer buf = ByteBuffer.allocate(4096);
+        ch.read(buf, buf, new CompletionHandler<>() {
+            public void completed(Integer n, ByteBuffer b) {
+                if (n == -1) {
+                    close(ch);
+                    return;
+                }
+                b.flip();
+                //System.out.println(new String(b.array(), StandardCharsets.UTF_8));
+                final String httpRequest = Function.getHTTPRequest(b);
+
+                //System.out.println(httpRequest);
+
+                if (httpRequest.isEmpty()) {
+                    close(ch);
+                    return;
+                }
+
+                final String httpVersion = Function.getHTTPVersion(httpRequest);
+                final String httpMethod = Function.getMethod(httpRequest);
+
+                final boolean isGET = httpMethod != null && httpMethod.equals("GET");
+                final boolean isPOST = httpMethod != null && httpMethod.equals("POST");
+                final boolean isHead = httpMethod != null && httpMethod.equals("HEAD");
+
+                if (!isGET && !isPOST && !isHead) {
+                    //System.out.println("[Debug] HTTPRequest送信");
+                    Function.sendHttpData(ch, new HttpHeader(httpVersion, 405, Function.contentType_textPlain, null, "*", Function.content_MethodNotAllowed, null));
+                    return;
+                }
+
+                final String URI = Function.getURI(httpRequest);
+                if (URI == null) {
+                    //System.out.println("[Debug] HTTPRequest送信");
+                    Function.sendHttpData(ch, new HttpHeader(httpVersion, 502, Function.contentType_textPlain, null, "*", Function.content_BadGateway, null));
+                    System.out.println(httpRequest);
+                    return;
+                }
+
+                final Matcher matcher = matcher_uri.matcher(URI);
+                final boolean ApiMatchFlag = URI.startsWith("/api/");
+                final boolean UrlMatchFlag = matcher.find();
+                final boolean VideoMatchFlag = URI.startsWith("/video");
+
+                Matcher matcher_range1 = matcher_http_range1.matcher(httpRequest);
+                Matcher matcher_range2 = matcher_http_range2.matcher(httpRequest);
+                final boolean RangeVideoFlag = matcher_range1.find();
+                final boolean RangeVideoFullFlag = matcher_range2.find();
+
+                if (ApiMatchFlag) {
+                    byte[] httpBody = null;
+                    for (NicoVRCAPI api : Function.APIList) {
+                        if (URI.startsWith(api.getURI())) {
+                            try {
+                                httpBody = api.Run(httpRequest, client).getBytes(StandardCharsets.UTF_8);
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                            Function.sendHttpData(ch, new HttpHeader(httpVersion, 200, Function.contentType_json, null, "*", httpBody, null));
+                            System.out.println("[API (" + Function.sdf.format(new Date()) + ")] " + URI + " ---> OK");
+                            Function.APIAccessLog.put(UUID.randomUUID().toString(), httpRequest);
+                            break;
+                        }
+                    }
+
+                    if (httpBody == null) {
+                        Function.sendHttpData(ch, new HttpHeader(httpVersion, 404, Function.contentType_textPlain, null, "*", Function.content_errorAPINotFound, null));
+                        System.out.println("[API (" + Function.sdf.format(new Date()) + ")] " + URI + " ---> Error");
+                        Function.APIAccessLog.put(UUID.randomUUID().toString(), httpRequest);
+                    }
+
+                    close(ch);
+                    return;
+                }
+
+                if (VideoMatchFlag || (RangeVideoFlag && !matcher_range1.group(1).equals("0"))) {
+                    getVideo.setHTTPRequest(httpRequest);
+                    getVideo.setURL(URI);
+                    getVideo.setHTTPSocket(ch);
+
+                    getVideo.run();
+                    close(ch);
+                    return;
+                }
+
+                if (UrlMatchFlag || RangeVideoFlag || RangeVideoFullFlag) {
+                    boolean b1 = matcher_uri_vi.matcher(URI).find();
+                    if (URI.startsWith("/proxy")) {
+                        getURL.setURL(URI.replaceFirst("/proxy/\\?", ""));
+                    } else if (b1) {
+                        getURL.setURL(URI.replaceFirst("vi=", "url="));
+                    }
+                    getURL.setHTTPRequest(httpRequest);
+
+                    getURL.setHTTPSocket(ch);
+
+                    getURL.run();
+                    close(ch);
+                    return;
+                }
+
+                Function.sendHttpData(ch, new HttpHeader(httpVersion, 404, Function.contentType_textPlain, null, "*", Function.content_NotFound, null));
+                close(ch);
+            }
+
+            public void failed(Throwable e, ByteBuffer b) {
+                close(ch);
+            }
+
+            private static void close(AsynchronousSocketChannel channel) {
+                try {
+                    if (channel != null && channel.isOpen()) {
+                        channel.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
     }
 }
